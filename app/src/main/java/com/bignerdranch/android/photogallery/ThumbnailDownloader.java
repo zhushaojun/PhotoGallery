43292420e514068fefb70ac106fa1f6e78992613
Shaojun Zhu <shj.zhu@gmail.com>
2017-04-05 21:07:41 +0800

    24.5.3

--- app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.java
@@ -1,7 +1,15 @@
 package com.bignerdranch.android.photogallery;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.Message;
 import android.util.Log;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Created by zhushaojun on 2017/4/5.
@@ -9,10 +17,41 @@ import android.util.Log;
 
 public class ThumbnailDownloader<T> extends HandlerThread {
     private static final String TAG = "ThumbnailDownloader";
+    private static final int MESSAGE_DOWNLOAD = 0;
+    private Handler mRequestHandler;
+    private ConcurrentMap<T, String> mRequestMap = new ConcurrentHashMap<>();
     private Boolean mHasQuit = false;
 
     public ThumbnailDownloader() {
         super(TAG);
+    }
+
+    @Override
+    protected void onLooperPrepared() {
+        mRequestHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == MESSAGE_DOWNLOAD) {
+                    T target = (T) msg.obj;
+                    Log.i(TAG, "Got a request for URL: " + mRequestMap.get(target));
+                    handleRequest(target);
+                }
+            }
+        };
+    }
+
+    private void handleRequest(final T target) {
+        try {
+            final String url = mRequestMap.get(target);
+            if (url == null) {
+                return;
+            }
+            byte[] bitmapBytes = new FlickrFetchr().getUrlBytes(url);
+            final Bitmap bitmap = BitmapFactory.decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
+            Log.i(TAG, "Bitmap created");
+        } catch (IOException ioe) {
+            Log.e(TAG, "Error downloading image", ioe);
+        }
     }
 
     @Override
@@ -23,5 +62,11 @@ public class ThumbnailDownloader<T> extends HandlerThread {
 
     public void queueThumbnail(T target, String url) {
         Log.i(TAG, "Got a URL: " + url);
+        if (url == null) {
+            mRequestMap.remove(target);
+        } else {
+            mRequestMap.put(target, url);
+            mRequestHandler.obtainMessage(MESSAGE_DOWNLOAD, target).sendToTarget();
+        }
     }
 }
