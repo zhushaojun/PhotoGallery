5df029a06272a8f65e80889d02045aff33a80aa1
Shaojun Zhu <shj.zhu@gmail.com>
2017-04-19 22:01:15 +0800

    25.1

--- app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
@@ -22,6 +22,16 @@ import java.util.List;
 public class FlickrFetchr {
     private static final String TAG = "FlickrFetchr";
     private static final String API_KEY = "2313397fd1678fa37e5c4b0801bb6554";
+    private static final String FETCH_RECENTS_METHOD = "flickr.photos.getRecent";
+    private static final String SEARCH_METHOD = "flickr.photos.search";
+    private static final Uri ENDPOINT = Uri
+            .parse("https://api.flickr.com/services/rest/")
+            .buildUpon()
+            .appendQueryParameter("api_key", API_KEY)
+            .appendQueryParameter("format", "json")
+            .appendQueryParameter("nojsoncallback", "1")
+            .appendQueryParameter("extras", "url_s")
+            .build();
 
     public byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
