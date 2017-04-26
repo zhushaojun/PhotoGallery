f46cc2028ca453318744b535456560f14cbfdae3
Shaojun Zhu <shj.zhu@gmail.com>
2017-04-26 21:11:41 +0800

    26.2

--- app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
@@ -3,6 +3,7 @@ package com.bignerdranch.android.photogallery;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.util.Log;
 
 /**
@@ -12,15 +13,26 @@ import android.util.Log;
 public class PollService extends IntentService {
     private static final String TAG = "PollService";
 
+    public PollService() {
+        super(TAG);
+    }
+
     public static Intent newIntent(Context context) {
         return new Intent(context, PollService.class);
     }
 
-    public PollService() {
-        super(TAG);
-    }
     @Override
     protected void onHandleIntent(Intent intent) {
+        if (!isNetworkAvailableAndConnected()) {
+            return;
+        }
         Log.i(TAG, "Received an intent: " + intent);
+    }
+
+    private boolean isNetworkAvailableAndConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        boolean isNetworkAvailable = cm.getActiveNetworkInfo() != null;
+        boolean isNetworkConnected = isNetworkAvailable && cm.getActiveNetworkInfo().isConnected();
+        return isNetworkConnected;
     }
 }
