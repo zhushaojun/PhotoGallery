7832083d5e9a22b1496a8242af2fc10b4efbe5d1
Shaojun Zhu <shj.zhu@gmail.com>
2017-05-03 21:38:56 +0800

    27.2.2

--- app/src/main/java/com/bignerdranch/android/photogallery/StartupReceiver.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/StartupReceiver.java
@@ -15,5 +15,8 @@ public class StartupReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i(TAG, "Received broadcast intent: " + intent.getAction());
+
+        boolean isOn = QueryPreferences.isAlarmOn(context);
+        PollService.setServiceAlarm(context, isOn);
     }
 }
